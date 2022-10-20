9b9bfbd468412ef1c310461fcea945f623ec5bb8
tania <taniaosano15@gmail.com>
2022-10-20 12:01:42 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
 
+    #getter method
+    def name= (name)
+        @name = name
+    end
+
+    #setter method
+    def name
+        @name
+    end
+
+    #getter method
+    def job= (job)
+        @job = job
+    end
+
+    #setter method
+    def job
+        @job
+    end
 end
